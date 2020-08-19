b07081357331c77340ab81b457d92f5cb271079d
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-08-19 14:50:50 -0600

    working on new way

--- app.js
+++ app.js
@@ -1,36 +1,65 @@
+let types = [
+{
+  name: "fire",
+  fire: "tie",
+  water: "lose",
+  grass: "win",
+  img: "https://images.unsplash.com/photo-1517594422361-5eeb8ae275a9"
+},
+{
+  name: "water",
+  fire: "win",
+  water: "tie",
+  grass: "lose",
+  img: "https://images.unsplash.com/photo-1479981280584-037818c1297d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
+},
+{
+  name: "grass",
+  fire: "lose",
+  water: "win",
+  grass: "tie",
+  img: "https://images.unsplash.com/photo-1494187570835-b188e7f0f26e"
+}
+]
 
 
-let str1 = ""
+
+
+
+
+
 
 function play(typeSelected){
  let random = Math.floor(Math.random() * 2.99)
-  str1 = typeSelected 
  console.log(random)
   drawResult()
  if(random == 0){
     switch(typeSelected){
-      case "Fire": drawResult("Fire", "tie"); break;
-      case "Water": drawResult("Fire", "win"); break;
-      case "Grass": drawResult("Fire", "lose"); break;
+      case "Fire": drawResult(typeSelected, "Fire", "tie"); break;
+      case "Water": drawResult(typeSelected, "Fire", "win"); break;
+      case "Grass": drawResult(typeSelected, "Fire", "lose"); break;
     }
   }
   else if(random == 1){
     switch(typeSelected){
-      case "Fire": drawResult("Water", "lose"); break;
-      case "Water": drawResult("Water", "tie"); break;
-      case "Grass": drawResult("Water", "win"); break;
+      case "Fire": drawResult(typeSelected, "Water", "lose"); break;
+      case "Water": drawResult(typeSelected, "Water", "tie"); break;
+      case "Grass": drawResult(typeSelected, "Water", "win"); break;
     }
   }
   else{
     switch(typeSelected){
-      case "Fire": drawResult("Grass", "win"); break;
-      case "Water": drawResult("Grass", "lose"); break;
-      case "Grass": drawResult("Grass", "tie"); break;
+      case "Fire": drawResult(typeSelected, "Grass", "win"); break;
+      case "Water": drawResult(typeSelected, "Grass", "lose"); break;
+      case "Grass": drawResult(typeSelected, "Grass", "tie"); break;
     }
   }
 }
 
-function drawResult(type, result){
+
+
+
+function drawResult(typePicked, typeFighting, result){
 const area = document.getElementById("resultsArea")
-area.innerHTML = ` <div class="col-12"><img src="//placehold.it/200x200" alt=""></div><div class="col-12"><h3>You chose ${str1} and faced ${type}, so you ${result}! </h3>`
+area.innerHTML = ` <div class="col-12"><img src="//placehold.it/200x200" alt=""></div><div class="col-12"><h3>You chose ${typePicked} and faced ${typeFighting}, so you ${result}! </h3>`
 }
