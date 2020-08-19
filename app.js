5e8bbdd1fcab1ab58f0fb8b1cb0869acb3aead24
SamuelJTaylor22 <sjtaylor1998@gmail.com>
2020-08-19 15:39:23 -0600

    done in slightly less monkey way

--- app.js
+++ app.js
@@ -1,26 +1,10 @@
-let types = [
-{
-  name: "fire",
-  fire: "tie",
-  water: "lose",
-  grass: "win",
-  img: "https://images.unsplash.com/photo-1517594422361-5eeb8ae275a9"
-},
-{
-  name: "water",
-  fire: "win",
-  water: "tie",
-  grass: "lose",
-  img: "https://images.unsplash.com/photo-1479981280584-037818c1297d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1350&q=80"
-},
-{
-  name: "grass",
-  fire: "lose",
-  water: "win",
-  grass: "tie",
-  img: "https://images.unsplash.com/photo-1494187570835-b188e7f0f26e"
+let strong = {
+
+  Fire: "Grass",
+  Water: "Fire",
+  Grass: "Water"
+
 }
-]
 
 
 
@@ -28,35 +12,58 @@ let types = [
 
 
 
+
+
+// function play(typeSelected){
+//  let random = Math.floor(Math.random() * 2.99)
+//  console.log(random)
+//   drawResult()
+//  if(random == 0){
+//     switch(typeSelected){
+//       case "Fire": drawResult(typeSelected, "Fire", "tie"); break;
+//       case "Water": drawResult(typeSelected, "Fire", "win"); break;
+//       case "Grass": drawResult(typeSelected, "Fire", "lose"); break;
+//     }
+//   }
+//   else if(random == 1){
+//     switch(typeSelected){
+//       case "Fire": drawResult(typeSelected, "Water", "lose"); break;
+//       case "Water": drawResult(typeSelected, "Water", "tie"); break;
+//       case "Grass": drawResult(typeSelected, "Water", "win"); break;
+//     }
+//   }
+//   else{
+//     switch(typeSelected){
+//       case "Fire": drawResult(typeSelected, "Grass", "win"); break;
+//       case "Water": drawResult(typeSelected, "Grass", "lose"); break;
+//       case "Grass": drawResult(typeSelected, "Grass", "tie"); break;
+//     }
+//   }
+// }
 
 function play(typeSelected){
- let random = Math.floor(Math.random() * 2.99)
- console.log(random)
-  drawResult()
- if(random == 0){
-    switch(typeSelected){
-      case "Fire": drawResult(typeSelected, "Fire", "tie"); break;
-      case "Water": drawResult(typeSelected, "Fire", "win"); break;
-      case "Grass": drawResult(typeSelected, "Fire", "lose"); break;
-    }
+  let random = Math.floor(Math.random() *(2.999))
+  let typeFighting = ""
+  let dictionaryPlace
+  let result = ""
+  switch(random){
+    case 0: typeFighting = "Fire"; dictionaryPlace = strong.Fire; break;
+    case 1: typeFighting = "Water"; dictionaryPlace = strong.Water; break;
+    case 2: typeFighting = "Grass"; dictionaryPlace = strong.Grass; break;
   }
-  else if(random == 1){
-    switch(typeSelected){
-      case "Fire": drawResult(typeSelected, "Water", "lose"); break;
-      case "Water": drawResult(typeSelected, "Water", "tie"); break;
-      case "Grass": drawResult(typeSelected, "Water", "win"); break;
-    }
+  if(typeSelected == typeFighting){
+    result = "tie"
+  }
+  else if(dictionaryPlace == typeSelected){
+    result = "win"
   }
   else{
-    switch(typeSelected){
-      case "Fire": drawResult(typeSelected, "Grass", "win"); break;
-      case "Water": drawResult(typeSelected, "Grass", "lose"); break;
-      case "Grass": drawResult(typeSelected, "Grass", "tie"); break;
-    }
+    result = "lose"
   }
+  
+drawResult(typeSelected, typeFighting, result)
+
 }
-
-
 
 
 function drawResult(typePicked, typeFighting, result){
